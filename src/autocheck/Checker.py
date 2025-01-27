e4875ba31a971da05762e439f5f359f3e593457e
ahn7157 <ahn7157@gmail.com>
2025-01-28 00:55:57 +0900

    fix: `btn_class_name` 매개변수 추가로 동적으로 버튼 동작 처리
    - 두 site의 버튼 이름이 달라졌음

--- src/autocheck/Checker.py
+++ src/autocheck/Checker.py
@@ -3,24 +3,29 @@ from selenium.webdriver.common.by import By
 from urllib import parse
 from autocheck.message.MessagePool import MessagePool
 
+LOGIN_ID = "login_id"
+PASSWORD = "password"
+BODY = "body"
+NETLOC = "netloc"
+URL_ZONE = "https://quasarzone.com/ajax/user/attendanceInsert"
+URL_PLAY = "https://quasarplay.com/ajax/user/attendanceInsert"
+BTN_CLASS_NAME_ZONE = "btn-login"
+BTN_CLASS_NAME_PLAY = "login-bt"
+
 
 class Checker:
-    def __init__(self):
-        self.__url_zone = 'https://quasarzone.com/ajax/user/attendanceInsert'
-        self.__url_play = 'https://quasarplay.com/ajax/user/attendanceInsert'
-
     def check(self, driver, user_data):
-        MessagePool().add_message(self.__check_core(driver, self.__url_zone, user_data))
-        MessagePool().add_message(self.__check_core(driver, self.__url_play, user_data))
+        MessagePool().add_message(self.__check_core(driver, URL_ZONE, user_data, BTN_CLASS_NAME_ZONE))
+        MessagePool().add_message(self.__check_core(driver, URL_PLAY, user_data, BTN_CLASS_NAME_PLAY))
 
     @staticmethod
-    def __check_core(driver, url, user):
+    def __check_core(driver, url, user, btn_class_name):
         driver.get(url)
-        driver.find_element(by=By.NAME, value="login_id").send_keys(user.id)
-        driver.find_element(by=By.NAME, value="password").send_keys(user.pw)
-        driver.find_element(by=By.CLASS_NAME, value="login-bt").click()
+        driver.find_element(by=By.NAME, value=LOGIN_ID).send_keys(user.id)
+        driver.find_element(by=By.NAME, value=PASSWORD).send_keys(user.pw)
+        driver.find_element(by=By.CLASS_NAME, value=btn_class_name).click()
 
-        result = json.loads(driver.find_element(by=By.TAG_NAME, value="body").text)
-        result['netloc'] = parse.urlparse(url).netloc
+        result = json.loads(driver.find_element(by=By.TAG_NAME, value=BODY).text)
+        result[NETLOC] = parse.urlparse(url).netloc
 
         return result
